943b2342d0c88934efd7732c4297e86570169218
KCGubler <64223373+KCGubler@users.noreply.github.com>
2021-10-27 13:53:42 -0600

    Upload files for state population project

---
+++ Project.js
@@ -0,0 +1,9 @@
+function year() {
+
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+    output.innerHTML = this.value;
+}}
