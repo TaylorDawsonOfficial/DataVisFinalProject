96f797f839c756f48e8921a538d5cfcf0357c1e7
Taylor R. Dawson <thecanadian0504@gmail.com>
2021-11-16 17:15:13 -0700

    Started work on bringing population data to front end

--- public/js/country.js
+++ public/js/country.js
@@ -2,27 +2,51 @@ class Country {
   constructor() {
     this.width = 960;
     this.height = 500;
-    this.selectedColor = "lightpink"
+    this.selectedColor = "lightpink";
     this.selectedState;
     this.data;
+    this.population_data = [];
 
     $.ajax({
       method: "get",
       url: "/data/topology",
-      success: data => {
+      success: (data) => {
         this.data = data[0];
-        let states = topojson.feature(this.data, this.data.objects.states).features;
+        let states = topojson.feature(
+          this.data,
+          this.data.objects.states
+        ).features;
         this.setupSvg(states);
-      }
+        assignPopData();
+      },
+    });
+
+    //Initialize population data
+    $.ajax({
+      method: "get",
+      url: "/data/population",
+      success: (pop_data) => {
+        for (let entry of pop_data) {
+          // console.log(entry.years, slider.value);
+          let year_pop = [];
+
+          for (let year of entry.years) {
+            year_pop.push({ year: year.year, population: year.population });
+          }
+
+          this.population_data[entry.state] = year_pop;
+        }
+      },
     });
   }
 
   fillState(id, color = "") {
-    $(`#${id}`).css('fill', color);
+    $(`#${id}`).css("fill", color);
   }
 
   setupSvg(paths) {
-    const svg = d3.select(".country-container")
+    const svg = d3
+      .select(".country-container")
       .append("svg")
       .attr("x", 0)
       .attr("y", 0)
@@ -32,13 +56,16 @@ class Country {
     const projection = d3.geoAlbersUsa();
     const path = d3.geoPath().projection(projection);
 
-    svg.selectAll(".state")
+    svg
+      .selectAll(".state")
       .data(paths)
       .enter()
       .append("path")
-      .attr("class", "state")
+      .attr("class", (d) => `state ${d.properties.name.replaceAll(" ", "")}`) //Adds both state and name of state as a class
       .attr("d", path)
-      .attr("id", (d) => { return d.id })
+      .attr("id", (d) => {
+        return d.id;
+      })
       .on("click", (d, i) => {
         let newState = i.properties.name;
         if (this.selectedState && this.selectedState !== newState) {
@@ -51,7 +78,9 @@ class Country {
   }
 }
 
-
+function assignPopData() {
+  console.log(Map.Country.population_data);
+}
 
 /*
     Sets up values on HTML page after page has loaded
@@ -59,20 +88,32 @@ class Country {
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    sliderYear();
+    initializeSlider();
   },
   false
 );
 
 /*
-  Updates the text for the slider whenever the slider moves
-*/
-function sliderYear() {
-  var slider = document.querySelector(".slider");
-  var output = document.querySelector(".yearOutput");
-  output.innerHTML = slider.value;
+      Slider code
+    */
+const slider = document.querySelector(".slider");
+const output = document.querySelector(".yearOutput");
 
-  slider.oninput = function () {
-    output.innerHTML = this.value;
-  };
+/*
+      Updates the text for the slider whenever the slider moves
+    */
+slider.oninput = function () {
+  output.innerHTML = this.value;
+  assignPopData();
+};
+
+function initializeSlider() {
+  slider.min = 1969;
+  slider.max = 2019;
+  slider.value = 1969;
+  output.innerHTML = slider.value;
+}
+
+function getSliderValue() {
+  return slider.value;
 }
--- public/js/state.js
+++ public/js/state.js
@@ -3,6 +3,13 @@ class State {
     this.stateObj = stateObj;
     this.state = stateObj.properties.name;
 
-    $('#selectedState').text(this.state);
+    $("#selectedState").text(this.state);
+    $.ajax({
+      method: "get",
+      url: "/data/historical-population",
+      success: (data) => {
+        console.log(data);
+      },
+    });
   }
 }
