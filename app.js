5f9e907d0c2829534e5ac9e70e43fd7986785a97
Carson Sorenson <carsonwsorenson@gmail.com>
2021-11-23 21:00:41 -0700

    restructuring app to separate state view

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const data = require('./app-data');
 const app = express();
+const fs = require('fs');
 const { init } = require("./db");
 
 const port = 3000;
@@ -11,6 +12,14 @@ app.use('/data', data);
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
+
+app.get('/', (req, res) => {
+  fs.readFile('./public/country-view.html', (err, data) => {
+    if (err) throw err;
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+})
 
 app.listen(port, () => {
   init();
